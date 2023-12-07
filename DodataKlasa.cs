0f23b3afd762db4208d9f7d9735cc9e6f512097a
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-07 03:22:57 +0100

    Add files via upload

---
+++ DodataKlasa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_and_External_Classes
+{
+    public class DodataKlasa
+    {
+        public int x = 0;
+        public string NekiString = "bla bla neki string";
+        public char NekiChar = 'Z';
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Objects_and_External_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           DodataKlasa PrviObjekat = new DodataKlasa();
+            Console.WriteLine(PrviObjekat.x);
+            Console.WriteLine(PrviObjekat.NekiString);
+            Console.WriteLine(PrviObjekat.NekiChar);
+
+            Console.WriteLine("##########");
+
+                DodataKlasa StotiObjekat = new DodataKlasa();
+            Console.WriteLine(StotiObjekat.x);
+            Console.WriteLine(StotiObjekat.NekiString);
+            Console.WriteLine(StotiObjekat.NekiChar);
+        }
+    }
+}
